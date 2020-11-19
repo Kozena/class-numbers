8246f379a98816865dfb1c24cf2301db7e2aaa4f
Kozena <62444291+Kozena@users.noreply.github.com>
2020-11-19 17:25:47 +0100

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,27 @@
+<?php
+
+include("class_numbers_k.php");
+
+var_dump(numbersBarboraKozena::round_BarboraKozena(420.69185436, 2));
+var_dump(numbersBarboraKozena::ceil_BarboraKozena(44.4));
+var_dump(numbersBarboraKozena::floor_BarboraKozena(66.6));
+var_dump(numbersBarboraKozena::sqrt_BarboraKozena(16));
+var_dump(numbersBarboraKozena::pow_BarboraKozena(2, 2));
+var_dump(numbersBarboraKozena::sin_BarboraKozena(90));
+var_dump(numbersBarboraKozena::cos_BarboraKozena(360));
+var_dump(numbersBarboraKozena::tan_BarboraKozena(0));
+var_dump(numbersBarboraKozena::asin_BarboraKozena(30));
+var_dump(numbersBarboraKozena::acos_BarboraKozena(60));
+var_dump(numbersBarboraKozena::atan_BarboraKozena(45));
+var_dump(numbersBarboraKozena::fmod_BarboraKozena(18.19, 3.3));
+var_dump(numbersBarboraKozena::max_BarboraKozena([11,22,444,666]));
+var_dump(numbersBarboraKozena::min_BarboraKozena([11,22,444,666]));
+var_dump(numbersBarboraKozena::abs_BarboraKozena(-6));
+var_dump(numbersBarboraKozena::odcitani_BarboraKozena(666, 222));
+var_dump(numbersBarboraKozena::scitani_BarboraKozena(222, 444));
+var_dump(numbersBarboraKozena::celociselne_deleni_BarboraKozena(22, 2));
+var_dump(numbersBarboraKozena::nasobeni_BarboraKozena(11, 2));
+var_dump(numbersBarboraKozena::number_format_BarboraKozena(6422.345, 2, ",", " "));
+
+?>
+
